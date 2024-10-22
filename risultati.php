3d0822ba8a8fe25c35a14b76a6d17bf94ccbe8df
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-22 16:32:49 +0200

    Aggiunte funzioni controllField(), putCorsi(), createTable() nella file risultati.php e il file css

--- risultati.php
+++ risultati.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>risultati.php</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
@@ -13,17 +14,69 @@
     $time = $_GET["time"];
     $transport = $_GET["transport"];
     $listCorsi = array();
-    $c1 = $_GET["corso1"];
-    $c2 = $_GET["corso2"];
-    $c3 = $_GET["corso3"];
-    
-
-    function controllCorsi($c, $lc, $i){
-        if (isset($c)) {
-            $lc[$i] = $c;
-        }
-        return $lc;
+    if (isset($_GET["corso1"])) {
+        $c1 = $_GET["corso1"];
+        $listCorsi[] = $c1;
     }
+    if (isset($_GET["corso2"])) {
+        $c2 = $_GET["corso2"];
+        $listCorsi[] = $c2;
+    }
+    if (isset($_GET["corso3"])) {
+        $c3 = $_GET["corso3"];
+        $listCorsi[] = $c3;
+    }
+    createTable($name, $surname, $date, $time, $transport, $listCorsi);
+
+
+    function controllField($s){
+        if(empty($s)){
+            return "Nessuna attività";
+        } else {
+            return $s;
+        }
+    }
+
+    function putCorsi($l){
+        if (empty($l)) {
+            return "Nessuna attività";
+        } else {
+            $i = 0;
+            echo "<td>";
+            foreach ($l as $c) {
+                if ($i == 0) {
+                    echo $c;
+                    $i++;
+                } else {
+                    echo ", $c"; 
+                }
+            }
+            echo "</td>";
+        }
+    }
+
+    function createTable($n, $s, $d, $t, $tr, $lc){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>Nome</th>";
+                echo "<th>Cognome</th>";
+                echo "<th>Data di nascita</th>";
+                echo "<th>Orario di uscita</th>";
+                echo "<th>Mezzo di trasporto</th>";
+                echo "<th>Corsi</th>";
+            echo "</tr>";
+            echo "<tr>";
+                echo "<td>" . $n = controllField($n) . "</td>";
+                echo "<td>" . $s = controllField($s) . "</td>";
+                echo "<td>" . $d = controllField($d) . "</td>";
+                echo "<td>" . $t = controllField($t) . "</td>";
+                echo "<td>" . $tr = controllField($tr) . "</td>";
+                putCorsi($lc);
+                
+            echo "</tr>";
+        echo "</table>";
+    }
+
     ?>
 </body>
 </html>
